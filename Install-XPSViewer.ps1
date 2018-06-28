fe9aa9f52885803d4e2ef7decdf54ed676471bea
Andrew M Taylor <andrew-m-taylor@uiowa.edu>
2018-06-28 16:16:36 -0500

    Added Help
    Added Help and Verbose strings. Tested, working

--- Install-XPSViewer.ps1
+++ Install-XPSViewer.ps1
@@ -1,9 +1,34 @@
+<#
+.SYNOPSIS
+    Installs XPS Viewer on target Windows workstations
+.DESCRIPTION
+    Windows 10 Build 1803 does not have XPS Viewer enabled. This script allows for users
+    to enable XPS Viewer on a single workstation or a list of workstations
+.EXAMPLE
+    PS C:\> Add-XPSViewer -ComputerName Client1
+    Tests to ensure Client1 is online, then enables XPS Viewer
+.EXAMPLE
+    PS C:\> Add-XPSViewer -Path "C:\WS\ClientList.txt"
+    Tests connectivitiy of all WS in list, then enables the XPS Viewer on all online WS. Displays a list of all offline workstations
+.EXAMPLE
+    PS C:\> Add-XPSViewer -Path "C:\WS\ClientList.txt" -TestRM
+    Tests connectivitiy of all WS in list, then enables the XPS Viewer on all online WS. Displays a list of all offline workstations. Also tests for WinRM emabled WS.
+.INPUTS
+    -ComputerName
+    -Path
+    -WSMan(Optional)
+.OUTPUTS
+   Offline.txt--All WS that failed Test-Connection
+   (Optional)NoRM.txt - All Online WS that WinRM failed
+.NOTES
+    Requires that WinRM is enabled on target WS.
+#>
 function Add-XPSViewer 
 {
     [CmdletBinding()]
     param 
     (
-        # Allows for a list of PC Names to check
+        # Allows for a list of PC Names to enable XPS Viewer
         [Parameter (Mandatory = $True, ParameterSetName = "Single WS")]
         [ValidateNotNullOrEmpty()]
         [string[]]
@@ -22,9 +47,11 @@ function Add-XPSViewer
     
     begin 
     {
+        #Casting arrays that will be used later in script
         $Online = New-Object System.Collections.Generic.List[System.Object]
         $Offline = New-Object System.Collections.Generic.List[System.Object]
         $NoRM = New-Object System.Collections.Generic.List[System.Object]
+        #Preparing input for script below.
         if($ComputerName -ne $Null)
         {
             Write-Debug "Preparing to process workstations"
@@ -45,12 +72,15 @@ function Add-XPSViewer
         #Test to see which workstations are offline
         foreach ($computer in $computers)
         {
+            Write-Verbose "Testing to see if $Computer is online"
             if (-not (Test-Connection -ComputerName $computer -ErrorAction SilentlyContinue -Count 1))
             {
+                Write-Verbose "$Computer is offline, removing from execution list"
                 $Offline.add($Computer)
             }
             else 
             {
+                Write-Verbose "$Computer is online"
                 $Online.add($Computer)
             }
         }
@@ -62,10 +92,12 @@ function Add-XPSViewer
             {
                 try
                 {
+                    Write-Verbose "Testing for WinRM on $O"
                     Test-WSMan -ComputerName $O
                 }
                 catch
                 {
+                    Write-Verbose "WinRM not enabled on $O, removeing from list"
                     $NoRM.add($O)
                     $Online.remove($O)
                 }
@@ -75,6 +107,7 @@ function Add-XPSViewer
         #Add XPS Viewer to the eligible machines
         foreach ($WS in $Online)
         {
+            Write-verbose "Enabling XPS Viewer on $WS"
             Invoke-Command -ComputerName $WS -ScriptBlock {DISM /Online /Add-Capability /CapabilityName:XPS.Viewer~~~~0.0.1.0}
             
         }
