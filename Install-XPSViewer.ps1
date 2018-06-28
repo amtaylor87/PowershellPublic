ff80d3422fe92250fe7e4e32b7deabe0174303ae
Andrew M Taylor <andrew-m-taylor@uiowa.edu>
2018-06-28 15:58:13 -0500

    Added Connection Tests
    Added connection test and optional WinRM Test

--- Install-XPSViewer.ps1
+++ Install-XPSViewer.ps1
@@ -1,6 +1,8 @@
-function Add-XPSViewer {
+function Add-XPSViewer 
+{
     [CmdletBinding()]
-    param (
+    param 
+    (
         # Allows for a list of PC Names to check
         [Parameter (Mandatory = $True, ParameterSetName = "Single WS")]
         [ValidateNotNullOrEmpty()]
@@ -11,10 +13,18 @@ function Add-XPSViewer {
         [Parameter (Mandatory = $True, ParameterSetName = "List of WS")]
         [ValidateNotNullOrEmpty()]
         [System.IO.FileInfo]
-        $Path
+        $Path,
+
+        #Switch to test for WinRM
+        [switch]
+        $TestRM
     )
     
-    begin {
+    begin 
+    {
+        $Online = New-Object System.Collections.Generic.List[System.Object]
+        $Offline = New-Object System.Collections.Generic.List[System.Object]
+        $NoRM = New-Object System.Collections.Generic.List[System.Object]
         if($ComputerName -ne $Null)
         {
             Write-Debug "Preparing to process workstations"
@@ -29,32 +39,54 @@ function Add-XPSViewer {
             exit
         }
     }
-    
-    process {
+
+    process 
+    {
+        #Test to see which workstations are offline
         foreach ($computer in $computers)
         {
-            Invoke-Command -ComputerName $computer -ScriptBlock {DISM /Online /Add-Capability /CapabilityName:XPS.Viewer~~~~0.0.1.0} 
+            if (-not (Test-Connection -ComputerName $computer -ErrorAction SilentlyContinue -Count 1))
+            {
+                $Offline.add($Computer)
+            }
+            else 
+            {
+                $Online.add($Computer)
+            }
+        }
+
+        #Then, test the online computers to ensure that WinRM is enabled, if switch is enabled
+        if($TestRM)
+        {
+            foreach ($O in $Online)
+            {
+                try
+                {
+                    Test-WSMan -ComputerName $O
+                }
+                catch
+                {
+                    $NoRM.add($O)
+                    $Online.remove($O)
+                }
+
+            }
+        }
+        #Add XPS Viewer to the eligible machines
+        foreach ($WS in $Online)
+        {
+            Invoke-Command -ComputerName $WS -ScriptBlock {DISM /Online /Add-Capability /CapabilityName:XPS.Viewer~~~~0.0.1.0}
+            
         }
     }
-    
-    end {
+    end 
+    {
+        $Offline | Out-File "$home\Documents\Offline.txt"
+        Invoke-Item "$home\Documents\Offline.txt"
+        if($TestRM)
+        {
+            $NoRm | Out-File "$home\Documents\NoRM.txt"
+            Invoke-Item "$home\Documents\NoRM.txt"
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-<#
-$Computers = Get-Content -Path "C:\users\schnedler_e\desktop\xps\computers.txt"
-foreach ($computer in $computers){
-Invoke-Command -ComputerName $computer -ScriptBlock {DISM /Online /Add-Capability /CapabilityName:XPS.Viewer~~~~0.0.1.0} 
-}
-#>
